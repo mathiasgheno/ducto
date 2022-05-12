80be318144d073117f5a4851a3596e2382020b58
mathias <mathiasgheno@gmail.com>
2022-05-12 10:12:47 -0300

    chore: creates the new project structure

---
+++ packages/validators/isString.js
@@ -0,0 +1,12 @@
+import log from 'loglevel';
+
+export const isString = (value) => {
+  const possibleString = value;
+  log.debug(`Receive ${possibleString} in isString`);
+  const isNotString = typeof possibleString !== 'string';
+  log.debug(`isNotString: ${isNotString}`);
+  if(isNotString) {
+    throw new Error(`Value ${possibleString} is not an string`);
+  }
+  return value;
+};
