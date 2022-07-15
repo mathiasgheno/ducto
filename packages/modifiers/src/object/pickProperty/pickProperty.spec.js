c60ee4cb6c5c59629bc45c4056ee1f0edbdb82ce
mathias <mathiasgheno@gmail.com>
2022-07-14 23:11:36 -0300

    test: fix test of pickProperty.spec.js

--- packages/modifiers/src/object/pickProperty/pickProperty.spec.js
+++ packages/modifiers/src/object/pickProperty/pickProperty.spec.js
@@ -3,5 +3,5 @@ import { pickProperty } from './pickProperty.js';
 test('should retorn movie with title', () => {
   const movie = {title: 'The Shawshank Redemption', year: 1994};
   const newMovie = pickProperty('title')(movie);
-  expect(newMovie).toEqual({title: 'The Shawshank Redemption'});
+  expect(newMovie).toEqual('The Shawshank Redemption');
 });
