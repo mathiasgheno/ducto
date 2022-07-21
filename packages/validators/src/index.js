914c7cb002ea3660c69ee63fbb665ba86b37b6c3
mathias <mathiasgheno@gmail.com>
2022-07-20 23:12:59 -0300

    feat: export hasSameLengthOf.js and hasSpecialCharacter

--- packages/validators/src/index.js
+++ packages/validators/src/index.js
@@ -5,3 +5,5 @@ export { isNumber } from './number/isNumber/isNumber.js';
 export { isNotUndefined } from './general/isNotUndefined/isNotUndefined.js';
 export { isNotNullish } from './general/isNotNullish/isNotNullish.js';
 export { isNotNull } from './general/isNotNull/isNotNull.js';
+export { hasSameLengthOf } from './general/hasSameLengthOf/hasSameLengthOf.js';
+export { hasSpecialCharacter } from './string/hasSpecialCharacter/hasSpecialCharacter.js';
