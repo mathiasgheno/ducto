81238ea67c298615b11ff2596ee397a97efdd1c2
mathias <mathiasgheno@gmail.com>
2022-08-18 07:25:24 -0300

    fix: uncomment code

--- examples/pipeline-operator-example/src/index.js
+++ examples/pipeline-operator-example/src/index.js
@@ -1,32 +1,18 @@
 const { isString, isNotNull, hasLessCharactersThan, hasMoreCharactersThan } = require('@ducto/validators');
 
-// function correct() {
-//   'mathias'
-//     |> isString(#)
-//     |> isNotNull(#)
-//     |> hasLessCharactersThan(100)(#)
-//     |> console.log(#)
-// }
-//
-// function error() {
-//   'mathias'
-//     |> isString(#)
-//     |> isNotNull(#)
-//     |> hasLessCharactersThan(100)(#)
-//     |> hasMoreCharactersThan(10)(#)
-//     |> console.log(#)
-// }
+function correct() {
+  'mathias'
+    |> isString(#)
+    |> isNotNull(#)
+    |> hasLessCharactersThan(100)(#)
+    |> console.log(#)
+}
 
-// const getString = () => 'mathias';
-//
-// getString()
-//   |> isString(#)
-//   |> isNotNull(#)
-//   |> hasLessCharactersThan(100)(#)
-
-
-const itens = ['mathias', 'nadine'];
-
-itens
-  |> isString.apply(this, #)
-  |> isNotNull.apply(this, #)
+function error() {
+  'mathias'
+    |> isString(#)
+    |> isNotNull(#)
+    |> hasLessCharactersThan(100)(#)
+    |> hasMoreCharactersThan(10)(#)
+    |> console.log(#)
+}
