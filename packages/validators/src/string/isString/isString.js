7463ef9dd086e8457b41c4b3596f665be3af7597
mathias <mathiasgheno@gmail.com>
2022-08-18 07:24:30 -0300

    refactor: improve structure of isString

--- packages/validators/src/string/isString/isString.js
+++ packages/validators/src/string/isString/isString.js
@@ -3,9 +3,9 @@ import log from 'loglevel';
 export const isString = (value) => {
   const possibleString = value;
   log.debug(`Receive ${possibleString} in isString`);
-  const isNotString = typeof possibleString !== 'string';
-  log.debug(`isNotString: ${isNotString}`);
-  if(isNotString) {
+const isNot = typeof possibleString !== 'string';
+  log.debug(`isNot: ${isNot}`);
+  if(isNot) {
     throw new Error(`Value is not an string`);
   }
   return value;
