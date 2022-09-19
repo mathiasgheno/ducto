a2300ae4631bf11b371288f9b10a72cb923b6d9a
mathias <mathiasgheno@gmail.com>
2022-09-18 23:19:50 -0300

    test: fix type error in implementation

--- packages/validators/src/object/thereIsProperty/thereIsProperty.spec.js
+++ packages/validators/src/object/thereIsProperty/thereIsProperty.spec.js
@@ -3,23 +3,19 @@ import { thereIsProperty } from './thereIsProperty.js';
 describe('thereIsProperty', () => {
   test('should throw if property is undefined', () => {
     expect(() => {
-      const obj = {
+      thereIsProperty('age')({
         name: 'nadine',
         age: undefined,
-      }
-      const validator = thereIsProperty('age')(obj);
-      validator();
+      })
     }).toThrowError();
   })
 
   test('should not throw if property is not undefined', () => {
     expect(() => {
-      const obj = {
+      thereIsProperty('age')({
         name: 'nadine',
         age: 10,
-      }
-      const validator = thereIsProperty('age')(obj);
-      validator();
-    }).not.toThrowError();
+      });
+    });
   })
 });
