36450011bda8e2ecde81a7fb515765c73e1eedf9
mathias <mathiasgheno@gmail.com>
2022-09-18 23:15:21 -0300

    fix: implementation and tests of isNumber

--- packages/validators/src/number/isNumber/isNumber.js
+++ packages/validators/src/number/isNumber/isNumber.js
@@ -3,7 +3,7 @@ import log from 'loglevel';
 export const isNumber = (value) => {
   log.info('Executing isNumber');
   log.debug(value);
-  if(Number.isNaN(value)) {
+  if(Number.isNaN(+value)) {
     throw new Error('Value is not a number');
   }
   return value;
--- packages/validators/src/number/isNumber/isNumber.spec.js
+++ packages/validators/src/number/isNumber/isNumber.spec.js
@@ -1,11 +1,21 @@
 import { isNumber } from './isNumber.js'
 
-describe('isNumber', () => {
+fdescribe('isNumber', () => {
   it('should not throw for a number', () => {
     expect(isNumber(1)).toBe(1)
   })
 
+  it('should not throw for a number as string', () => {
+    expect(isNumber('10')).toBe('10')
+  })
+
+  it('should not throw for a science number as string', () => {
+    expect(isNumber('10e10')).toBe('10e10')
+  })
+
   it('should throw for a non-number', () => {
-    expect(() => isNumber('1')).toThrowError('Value is not a number')
+    expect(() => isNumber('1e')).toThrowError('Value is not a number')
+    expect(() => isNumber('e')).toThrowError('Value is not a number')
+    expect(() => isNumber('a')).toThrowError('Value is not a number')
   })
 })
