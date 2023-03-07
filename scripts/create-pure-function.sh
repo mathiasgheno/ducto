4f07f925ee0002001bca89575531a21cb521d29d
mathias <mathiasgheno@gmail.com>
2023-03-06 23:19:39 -0300

    chore(scripts): change name of script to create-pure-function.sh

--- scripts/create-pure-function.sh
+++ scripts/create-pure-function.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
-# create-validator myValidator
+# create-pure-function myFunctionName
+# cpf myFunctionName
 
 mkdir "$1"
 touch "$1"/"$1".js
 touch "$1"/README.md
 touch "$1"/"$1".spec.js
+touch "$1"/"$1".types.js
